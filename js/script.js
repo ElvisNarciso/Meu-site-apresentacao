9fbe1fa51b2b745af625d16f0fb438b4cc888ee4
ElvisNarciso <elvisnarciso222@gmail.com>
2022-08-02 09:59:40 -0300

    Primeiro

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*=============== Typing animation ================= */
+var typed = new Typed(".typing",{
+    strings:["","Desenvolvimento Web","HTML","CSS","JavaScript","Azure Data Fundamentals"],
+    typeSpedd:100,
+    backSpeed:90,
+    loop:true
+})
